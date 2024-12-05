7d288f542675ff265b58efa3b3efea70293b8099
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-12-04 21:01:45 -0600

    Create Banking_System_impl.hpp

---
+++ Banking_System_impl.hpp
@@ -0,0 +1,16 @@
+#ifndef BANKING_SYSTEM_IMPL_HPP_
+#define BANKING_SYSTEM_IMPL_HPP_
+
+#include "banking_system.hpp"
+#include <unordered_map>
+
+class BankingSystemImpl : public BankingSystem {
+        private:
+                std::unordered_map<std::string, int> accounts;
+        public:
+                bool CreateAccount(int timestamp, const std::string&account_id);
+                std::optional<int> Deposit(int timestamp, const std::string& account_id, int amount);
+                std::optional<int> Transfer(int timestamp, const std::string& source_account_id, const std::string& target_account_id, int amount);     
+};
+
+#endif  // BANKING_SYSTEM_IMPL_HPP_
